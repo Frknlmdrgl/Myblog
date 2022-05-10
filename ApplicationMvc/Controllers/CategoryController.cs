606ddca51bf3286a5b8c7df1913d526f65337226
frknlmdrgl <furkanalemdaroglu@outlook.com>
2022-05-10 03:53:10 -0700

    Add project files.

---
+++ ApplicationMvc/Controllers/CategoryController.cs
@@ -0,0 +1,44 @@
+﻿using ApplicationMvc.Data;
+using ApplicationMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationMvc.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+        //Get
+        public IActionResult Create()
+        {
+             return View();
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        { 
+            if(obj.Name == obj.SecondName)
+            {
+                ModelState.AddModelError("Name", "İsminiz ile Soyisminiz aynı olamaz");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Mesajınız başarılı bir şekilde gönderildi";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+            
+        }
+    }
+}
---
+++ ApplicationMvc/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApplicationMvc.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; } 
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string SecondName { get; set; }
+        [Required]
+        
+        public string Description { get; set; }
+        [Required]
+        [Range(15, 70,ErrorMessage ="Yaşınız 15 ile 70 arasında olmalıdır")]
+        public int Age { get; set; }
+        public DateTime CreatedDate { get; set; }= DateTime.Now;
+    }
+}
